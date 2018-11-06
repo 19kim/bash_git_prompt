8a2aee86651955f0ff4f2b895f5d4936d3f3c1ca
kim19(pi) <kimig009@gmail.com>
2018-11-06 17:33:16 +0900

    checknotpush function

--- bash_git_prompt.bash
+++ bash_git_prompt.bash
@@ -1,3 +1,12 @@
 getNowBranch() {
   git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'
 }
+CheckNotPush() {
+  pushlog=git log --branches --not --remotes 2> /dev/null
+  
+  if [ -z pushlog ]; then
+    echo "비었음."
+  else
+    echo "필요함"
+  fi
+}
